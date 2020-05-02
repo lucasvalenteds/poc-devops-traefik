5f8c56dbc7ed2a57b9e1c115a5541396df9af93c
Lucas <lucasvalenteds@gmail.com>
2020-05-02 15:42:50 -0300

    Remove unnecessary `async` usage

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import * as HTTP from "http";
 
 const port = process.env.PORT;
 
-const server = HTTP.createServer(async (_, response) => {
+const server = HTTP.createServer((_, response) => {
   response.statusCode = 200;
   response.write(JSON.stringify({ message: "Hello World" }));
   response.end(() => console.info("%s: Request received", new Date()));
